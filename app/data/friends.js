07fcbbc05ce3f41317bedaa3f93f2c26308f94d4
Kris <kris.bierma@gmail.com>
2018-04-26 21:19:55 -0500

    added one more user!

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,6 +20,11 @@ var allUsers=[
         name:"M. Average",
         photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqrZwY79whqI6yQhGQEMmfBrcuT-yXWbJL-5L1rRP-kU_qCeH5",
         scores:["3","3","3","3","3","3","3","3","3","3"]
+    },
+    {
+        name:"David",
+        photo:"https://media.licdn.com/dms/image/C4E03AQFSrVtrLrZTgA/profile-displayphoto-shrink_800_800/0?e=1529982000&v=beta&t=13mPZHxoto3gjokv-14d-tj2pBEdDrq5jZ55Bc26-nc",
+        scores:["5","5","3","3","4","4","5","3","4","5"]
     }
 ];
 
